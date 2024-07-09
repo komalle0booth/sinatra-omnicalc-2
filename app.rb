d720f2291ff0dba3871832e4858b7cc2e6f05398
komalle0booth <komalle0@chicagobooth.edu>
2024-07-09 04:05:46 +0000

    Calculator Debug #1

--- app.rb
+++ app.rb
@@ -2,15 +2,17 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  redirect("/add")
+  erb(:add_form)
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
-  @first_num = params.fetch("first_num").to_f
+  @first_num = params.fetch("add_first_number").to_f
+
+  @second_num = params.fetch("add_second_number").to_f
 
   @result = @first_num + @second_num
   erb(:add_result)
@@ -21,17 +23,25 @@ get("/subtract") do
 end
 
 get("/wizard_subtract") do
-  @first_num = params.fetch("first_num").to_f
-  @second_num = params.fetch("second_num").to_f
+  @first_num = params.fetch("sub_first_number").to_f
 
+  @second_num = params.fetch("sub_second_number").to_f
+
+  @result = @second_num - @first_num
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("mult_first_number").to_f
+
+  @second_num = params.fetch("mult_second_number").to_f
+
+  @result = (@first_num)*(@second_num)
+
   erb(:mult_result)
 end
 
@@ -40,5 +50,11 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("div_first_number").to_f
+
+  @second_num = params.fetch("div_second_number").to_f
+
+  @result = (@first_num)/(@second_num)
+
   erb(:div_result)
 end
